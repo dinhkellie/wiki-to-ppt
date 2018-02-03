e406ad6b4c640f899377db498b0b6e821d8c60c9
Nicki Feldbaum <nickifeldbaum@gmail.com>
2018-02-03 18:57:45 -0500

    things work again in wiki parsing!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 var app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000
+var wtf = require('wtf_wikipedia');
 
 app.use(express.static('public'));
 
@@ -17,14 +18,6 @@ else {
   PptxGenJS = require("pptxgenjs");
 }
 
-express()
-.use(express.static(path.join(__dirname, 'public')))
-.use(('/bower_components',  express.static(__dirname + '/bower_components')))
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/index'))
-.get('/ppt', (req, res) => res.render('pages/download'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // From w3resource
 function strip(str) {
@@ -56,151 +49,155 @@ function makeGrammaticallyCorrectList(str){
   return str;
 }
 
-// index page
-app.get('/', function(req, res) {
-  var wtf = require('wtf_wikipedia');
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(('/bower_components',  express.static(__dirname + '/bower_components')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/ppt', (req, res) => res.render('pages/download'))
+  .get('/', (req, res) => {
+      wtf.from_api('Bill Clinton', 'en', function(markup) {
+        var data = wtf.parse(markup);
+        var full_json = data.infoboxes[0].data;
+        // console.log(full_json);
 
-  wtf.from_api('Bill Clinton', 'en', function(markup) {
-    var data = wtf.parse(markup);
-    var full_json = data.infoboxes[0].data;
-    // console.log(full_json);
-
-    try {
-      var birth_date = full_json.birth_date.text;
-      birth_date = birth_date.split("|");
-      for (i=0;i<birth_date.length; i++){
-        if (!isNaN(birth_date[i])){
-          birth_date = strip(birth_date[i]);
-          break;
-        }
-      }
-    } catch(error){
-      var birth_date = null;
-    }
-    try {
-      var death_date = full_json.death_date.text;
-      death_date = death_date.split("|");
-      for (i=0;i<death_date.length; i++){
-        if (!isNaN(death_date[i])){
-          death_date = strip(death_date[i]);
-          break;
-        }
-      }
-    } catch(error){
-      var death_date = null;
-    }
-
-    try {
-      var birth_name = strip(full_json.birth_name.text);
-    } catch(error){
-      var birth_name = null;
-    }
-
-    try {
-      var birth_place = strip(full_json.birth_place.text);
-    } catch(error){
-      var birth_place = null;
-    }
-
-    try {
-      var pseudonym = full_json.pseudonym.text;
-      pseudonym = strip(makeGrammaticallyCorrectList(pseudonym));
-    } catch(error){
-      var pseudonym = null;
-    }
-
-    try {
-      var image_name = strip(full_json.image.text);
-    } catch(error){
-      var image_name = null;
-    }
-
-    try {
-      var education = full_json.education.text;
-      var indexOfParen = education.indexOf('(');
-      var indexOfPipe = education.indexOf('|');
-      // if (index != -1){
-      //   highest_degree = education.slice(index);
-      //   highest_degree = highest_degree.replace('(','');
-      //   highest_degree = highest_degree.replace(')','');
-      //   highest_degree = highest_degree.split(',');
-      //   for (var j=0;j<highest_degree.length;j++){
-      //     if (isNaN(highest_degree[j])){
-      //       highest_degree = strip(highest_degree[j]);
-      //       break;
-      //     }
-      //   }
-      education = strip(education.slice(indexOfPipe+1,indexOfParen));
-      // }
-    } catch(error){
-      var education = null;
-      // var highest_degree = null;
-    }
-
-    try {
-      var occupation = full_json.occupation.text;
-      occupation = strip(makeGrammaticallyCorrectList(occupation));
-    } catch(error){
-      try {
-        var occupation = full_json.office.text;
-        occupation = strip(makeGrammaticallyCorrectList(occupation));
-      } catch(error){
-        var occupation = null;
-      }
-    }
-
-    try {
-      var notable_for = strip(full_json.notableworks.text);
-    } catch(error){
-      try {
-        var notable_for = strip(full_json.known_for.text);
-      } catch(error) {
-        var notable_for = null;
-      }
-    }
-
-    try {
-      var website = strip(full_json.website.text);
-      var webList = website.split('|');
-      var newListString = "";
-      var counter = 0;
-      for (var i=0;i<webList.length;i++){
-        if (webList[i].includes('.')){
-          if (counter == 0){
-            newListString = newListString.concat(webList[i]);
-          } else {
-            newListString = newListString.concat(", ",webList[i]);
+        try {
+          var birth_date = full_json.birth_date.text;
+          birth_date = birth_date.split("|");
+          for (i=0;i<birth_date.length; i++){
+            if (!isNaN(birth_date[i])){
+              birth_date = strip(birth_date[i]);
+              break;
+            }
           }
-          counter++;
+        } catch(error){
+          var birth_date = null;
         }
-      }
-      console.log(counter);
-      if (counter>2){
-        var lastComma = newListString.lastIndexOf(',');
-        website = "".concat(newListString.slice(0,lastComma+1)," and",newListString.slice(lastComma+1));
-      } else {
-        website = newListString;
-      }
-    } catch(error) {
-      var website = null;
-    }
+        try {
+          var death_date = full_json.death_date.text;
+          death_date = death_date.split("|");
+          for (i=0;i<death_date.length; i++){
+            if (!isNaN(death_date[i])){
+              death_date = strip(death_date[i]);
+              break;
+            }
+          }
+        } catch(error){
+          var death_date = null;
+        }
+
+        try {
+          var birth_name = strip(full_json.birth_name.text);
+        } catch(error){
+          var birth_name = null;
+        }
+
+        try {
+          var birth_place = strip(full_json.birth_place.text);
+        } catch(error){
+          var birth_place = null;
+        }
+
+        try {
+          var pseudonym = full_json.pseudonym.text;
+          pseudonym = strip(makeGrammaticallyCorrectList(pseudonym));
+        } catch(error){
+          var pseudonym = null;
+        }
+
+        try {
+          var image_name = strip(full_json.image.text);
+        } catch(error){
+          var image_name = null;
+        }
+
+        try {
+          var education = full_json.education.text;
+          var indexOfParen = education.indexOf('(');
+          var indexOfPipe = education.indexOf('|');
+          // if (index != -1){
+          //   highest_degree = education.slice(index);
+          //   highest_degree = highest_degree.replace('(','');
+          //   highest_degree = highest_degree.replace(')','');
+          //   highest_degree = highest_degree.split(',');
+          //   for (var j=0;j<highest_degree.length;j++){
+          //     if (isNaN(highest_degree[j])){
+          //       highest_degree = strip(highest_degree[j]);
+          //       break;
+          //     }
+          //   }
+          education = strip(education.slice(indexOfPipe+1,indexOfParen));
+          // }
+        } catch(error){
+          var education = null;
+          // var highest_degree = null;
+        }
+
+        try {
+          var occupation = full_json.occupation.text;
+          occupation = strip(makeGrammaticallyCorrectList(occupation));
+        } catch(error){
+          try {
+            var occupation = full_json.office.text;
+            occupation = strip(makeGrammaticallyCorrectList(occupation));
+          } catch(error){
+            var occupation = null;
+          }
+        }
+
+        try {
+          var notable_for = strip(full_json.notableworks.text);
+        } catch(error){
+          try {
+            var notable_for = strip(full_json.known_for.text);
+          } catch(error) {
+            var notable_for = null;
+          }
+        }
+
+        try {
+          var website = strip(full_json.website.text);
+          var webList = website.split('|');
+          var newListString = "";
+          var counter = 0;
+          for (var i=0;i<webList.length;i++){
+            if (webList[i].includes('.')){
+              if (counter == 0){
+                newListString = newListString.concat(webList[i]);
+              } else {
+                newListString = newListString.concat(", ",webList[i]);
+              }
+              counter++;
+            }
+          }
+          console.log(counter);
+          if (counter>2){
+            var lastComma = newListString.lastIndexOf(',');
+            website = "".concat(newListString.slice(0,lastComma+1)," and",newListString.slice(lastComma+1));
+          } else {
+            website = newListString;
+          }
+        } catch(error) {
+          var website = null;
+        }
 
 
-    console.log("Year of birth:",birth_date);
-    console.log("Year of death:",death_date);
-    console.log("Name at birth:",birth_name);
-    console.log("Location of birth:",birth_place);
-    console.log("Pseudonyms:",pseudonym);
-    console.log("Image names:",image_name);
-    console.log("Education:",education);
-    // console.log("Degree earned:",highest_degree);
-    console.log("Occupation:",occupation);
-    console.log("Notable for:",notable_for);
-    console.log("Website:",website);
+        console.log("Year of birth:",birth_date);
+        console.log("Year of death:",death_date);
+        console.log("Name at birth:",birth_name);
+        console.log("Location of birth:",birth_place);
+        console.log("Pseudonyms:",pseudonym);
+        console.log("Image names:",image_name);
+        console.log("Education:",education);
+        // console.log("Degree earned:",highest_degree);
+        console.log("Occupation:",occupation);
+        console.log("Notable for:",notable_for);
+        console.log("Website:",website);
 
-    // console.log(data.infoboxes[0]);
-    res.render('pages/index', {
-      tagline: full_json
-    });
-  });
-});
+        // console.log(data.infoboxes[0]);
+        // res.render('pages/index', {
+        //   tagline: full_json
+        // });
+      });
+    return res.render('pages/index') })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
