f8a4cceffdec5d61d1c72b9253510fa50d3360f0
Kellie Dinh <dinhkellie@yahoo.com>
2018-02-03 11:55:59 -0500

    ppt button

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var pptx = require('pptxgenjs');
+// var pptx = require('pptxgenjs');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
