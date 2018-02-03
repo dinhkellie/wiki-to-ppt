84634b10b0fece48910a3b89d75b3d72fc341d56
Kellie Dinh <dinhkellie@yahoo.com>
2018-02-03 14:07:37 -0500

    working ppt app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const fs = require('fs');
+
+var PptxGenJS;
+if (fs.existsSync('../dist/pptxgen.js')) {
+	PptxGenJS = require('bower_components/pptxgenjs/dist/pptxgen.js');
+	if (gConsoleLog) console.log('-=TEST MODE=- (bower_components/pptxgenjs/dist/pptxgen.js)');
+}
+else {
+	PptxGenJS = require("pptxgenjs");
+}
+
+var pptx = new PptxGenJS();
+var slide = pptx.addNewSlide();
+slide.addText('sfhalsjflj', { font_face: "Gothic", x:1.5, y:1.5, font_size:18, color:'363636' });
+pptx.save('Sample Presentation');
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const fs = require('fs');
+var app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000
 
-// var pptx = require('pptxgenjs');
+app.use(express.static('public'));
+
+var tools = require('./app');
+
+var PptxGenJS;
+if (fs.existsSync('../dist/pptxgen.js')) {
+	PptxGenJS = require('bower_components/pptxgenjs/dist/pptxgen.js');
+	if (gConsoleLog) console.log('-=TEST MODE=- (bower_components/pptxgenjs/dist/pptxgen.js)');
+}
+else {
+	PptxGenJS = require("pptxgenjs");
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,9 +24,10 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-function createPPT() {
-  pptx = new PptxGenJS();
-  var slide = pptx.addNewSlide();
-  slide.addText('Hello World!', { font_face: "Gothic", x:1.5, y:1.5, font_size:18, color:'363636' });
-  pptx.save('Sample Presentationa');
-}
+express().get('/ppt', function(req, res) {
+    res.render('pages/download');
+    var pptx = new PptxGenJS();
+    var slide = pptx.addNewSlide();
+    slide.addText('Hello World!', { font_face: "Gothic", x:1.5, y:1.5, font_size:18, color:'363636' });
+    pptx.save('Sample Presentation');
+});
