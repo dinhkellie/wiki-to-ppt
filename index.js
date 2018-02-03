5a93dedebab66105068a2ec6964c84e6d96e07d1
Kellie Dinh <dinhkellie@yahoo.com>
2018-02-03 11:52:27 -0500

    require pptxgenjs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var pptx = require('pptxgenjs');
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
