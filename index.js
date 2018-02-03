e155e365f92bdc4d9d48f28392880720270f4020
Kellie Dinh <dinhkellie@yahoo.com>
2018-02-03 14:42:41 -0500

    fix routes

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/ppt', (req, res) => res.render('pages/download'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.get('/ppt', function(req, res) {
-    res.render('pages/download');
     var pptx = new PptxGenJS();
     var slide = pptx.addNewSlide();
     slide.addText('Hello World!', { font_face: "Gothic", x:1.5, y:1.5, font_size:18, color:'363636' });
